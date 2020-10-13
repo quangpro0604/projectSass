efb34a0efb89628c6c29ffe867deba1d226b66e2
quangpro0604 <hoangminhquang235@gmail.com>
2020-10-13 10:32:31 +0700

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+/// Q&A 
+
+
+// function panel(){
+// //      phần tự làm
+// //     document.getElementById("qua_Title").style.display = 'block';
+// //     document.getElementById("panel").classList.toggle("panel").style.display = 'block';
+
+
+//     //  phần làm sai 
+//     // document.getElementById("qua_Title").style.display = 'block';
+//     // document.getElementById("qua").classList.toggle("open");
+// }
+
+
+/// END Q&A
